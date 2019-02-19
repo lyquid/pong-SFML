af24f8b53fb8dd2dadb93fc483d604755c4951d9
Alex <ketepincho@gmail.com>
2019-02-20 00:23:15 +0100

    Tidied player class

--- pong-SFML/classes/player.h
+++ pong-SFML/classes/player.h
@@ -10,6 +10,9 @@ class Player {
   Player(sf::String name, int number) {
     name_ = name;
     number_ = number;
+    score_ = 0;
+    size_ = sf::Vector2f(10.f, 128.f);
+    speed_ = 400.f;
     shape_.setSize(size_);
     shape_.setOrigin(size_ / 2.f);
   }
@@ -28,13 +31,13 @@ class Player {
   void setPosition(sf::Vector2f position);
 
  private: 
-  sf::String name_ = "";
-  int number_ = NULL;
+  sf::String name_;
+  int number_;
   sf::Vector2f position_;
-  int score_ = 0;
+  int score_;
   sf::RectangleShape shape_;
-  sf::Vector2f size_ = sf::Vector2f(10.f, 128.f);
-  float speed_ = 400.f;
+  sf::Vector2f size_;
+  float speed_;
 };
 
 #endif  // PONG_SFML_CLASSES_PLAYER_H_

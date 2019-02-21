cf90b343c0d21d9d80511c26c0fa8f0c4181ba3e
Alex <ketepincho@gmail.com>
2019-02-21 21:56:55 +0100

    Added game modes

--- pong-SFML/config.h
+++ pong-SFML/config.h
@@ -1,6 +1,11 @@
 #ifndef PONG_SFML_CONFIG_H_
 #define PONG_SFML_CONFIG_H_
 
+enum GameModes {
+  k1Player,
+  k2Players
+};
+
 const sf::String kAppName = "SFML Pong";
 const sf::String kAppVersion = "0.0"; 
 const float kPi = 3.14159f;
